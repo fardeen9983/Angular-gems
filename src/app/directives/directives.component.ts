bde4f3eab306701fc790177bca876dde6c5a3e7e
Fardeen Khan <khanfardeen9983@gmail.com>
2019-08-22 22:19:23 +0530

    ngFor, ngIf, ngStyle and ngClass revised

--- src/app/directives/directives.component.ts
+++ src/app/directives/directives.component.ts
@@ -6,11 +6,13 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./directives.component.css']
 })
 export class DirectivesComponent implements OnInit {
+  odds = [1, 3, 5, 7, 9];
+  evens = [2, 4, 6, 8, 10];
+  onlyOdd = false;
 
   constructor() {
   }
 
   ngOnInit() {
   }
-
 }
