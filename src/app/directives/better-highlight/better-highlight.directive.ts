2922093fa78d3d07161993487f8914e323547d43
Fardeen Khan <khanfardeen9983@gmail.com>
2019-08-22 23:27:35 +0530

    Custom property binding in custom directives

--- src/app/directives/better-highlight/better-highlight.directive.ts
+++ src/app/directives/better-highlight/better-highlight.directive.ts
@@ -1,23 +1,30 @@
-import {Directive, ElementRef, HostBinding, HostListener, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
-export class BetterHighlightDirective {
+export class BetterHighlightDirective implements OnInit {
+  @Input() defaultColor = 'transparent';
+  @Input() defaultHighlightColor = 'blue';
   @HostBinding('style.color') color = 'black';
+  @HostBinding('style.backgroundColor') backgroundColor;
 
   constructor(private element: ElementRef, private renderer: Renderer2) {
   }
 
+  ngOnInit() {
+    this.backgroundColor = this.defaultColor;
+  }
+
   @HostListener('mouseenter')
   mouseOver(eventData: Event) {
-    this.renderer.setStyle(this.element.nativeElement, 'background-color', 'blue');
+    this.renderer.setStyle(this.element.nativeElement, 'background-color', this.defaultHighlightColor);
     this.color = 'white';
   }
 
   @HostListener('mouseleave')
   mouseLeave(eventData: Event) {
-    this.renderer.setStyle(this.element.nativeElement, 'background-color', 'transparent');
+    this.renderer.setStyle(this.element.nativeElement, 'background-color', this.defaultColor);
     this.color = 'black';
   }
 }
